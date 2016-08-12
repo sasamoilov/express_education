f2bf4311a273f08eb1dc6d9bb7ea3c171bd3452a
Sergei Samoilov <samoilov@smartway.today>
2016-08-12 15:37:28 +0300

    Пропущенные файлы

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-/**
- * Created by sergeisamoilov on 12/08/16.
- */
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
+app.get('/index2', function(req, res) {
+    res.render('index2');
+});
+
+module.exports = app;
